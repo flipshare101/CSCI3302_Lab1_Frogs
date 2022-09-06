bdc569ed3393cffdd6f7a972eaee4288c29e3e5e
Nathan Ovadia <55854455+flipshare101@users.noreply.github.com>
2022-09-06 17:53:07 -0600

    now moves to first box, turns around, moves to second box, and trys to find left wall

--- my_controller.py
+++ my_controller.py
@@ -1,7 +1,7 @@
 from controller import Robot, DistanceSensor, Motor
 
 # time in [ms] of a simulation step
-TIME_STEP = 64
+TIME_STEP = 10
 
 MAX_SPEED = 6.28
 
@@ -19,6 +19,7 @@ for i in range(8):
     ps.append(robot.getDevice(psNames[i]))
     ps[i].enable(TIME_STEP)
 
+
 leftMotor = robot.getDevice('left wheel motor')
 rightMotor = robot.getDevice('right wheel motor')
 leftMotor.setPosition(float('inf'))
@@ -26,22 +27,16 @@ rightMotor.setPosition(float('inf'))
 leftMotor.setVelocity(0.0)
 rightMotor.setVelocity(0.0)
 
-state = move_straight()
-while robot.step(TIME_STEP) != -1:
-    psValues = []
-    for i in range(8):
-        psValues.append(ps[i].getValue())
-    state = state.update()
-
 class move_straight:
 
     def __init__(self, s_number): #default action of state
         self.step = s_number
         leftMotor.setVelocity(3)
         rightMotor.setVelocity(3)
+        print("move_straight")
     
     def update(self): #condition to change state
-        if psValues[7] < 34 or psValues[0] < 34: # obstical in front
+        if psValues[7] > 450 or psValues[0] > 450: # obstical in front
             if self.step == 0:
                 return u_turn()
             else:
@@ -54,11 +49,13 @@ class move_straight:
 class u_turn:
 
      def __init__(self): #turn the robot 180
-        leftMotor.setVelocity(-MAX_SPEED)
-        rightMotor.setVelocity(MAX_SPEED)
+        leftMotor.setVelocity(-0.5)
+        rightMotor.setVelocity(0.5)
+        print("u_turn")
         
      def update(self):
-         if psValues[3] < 34 or psValues[4] < 34: #obstical behind
+         print("PS3: ", psValues[3], "PS4", psValues[4])
+         if psValues[3] > 350 and psValues[4] > 350: #obstical behind
              return move_straight(1)
          else:
              return self
@@ -68,9 +65,10 @@ class find_left_wall:
     def __init__(self): #turn the robot
         leftMotor.setVelocity(-1)
         rightMotor.setVelocity(1)
+        print("find_left_wall")
         
     def update(self):
-        if psValues[5] < 34:
+        if psValues[5] > 100:
             return move_along_left_wall()
         else:
             return self
@@ -79,17 +77,25 @@ class move_along_left_wall:
    def __init__(self):
        leftMotor.setVelocity(3)
        rightMotor.setVelocity(3)
+       print("move_along_left_wall")
     
    def update(self):
-        if psValues[5] > 34:
+        if psValues[5] < 100:
             return rest()
         else:
             return self
                 
 class rest:
     def __init__(self):
-        print('done')
+        print('rest')
 
     def update(self):
         stop_motors()
         return self
+        
+state = move_straight(0)
+while robot.step(TIME_STEP) != -1:
+    psValues = []
+    for i in range(8):
+        psValues.append(ps[i].getValue())
+    state = state.update()
